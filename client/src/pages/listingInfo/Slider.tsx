5ca3464b41792c6976ef539e2597043da0e7eb29
akbiv <akbivash@gmail.com>
2023-11-18 15:33:03 +0545

    Remove unnecessary code

--- client/src/pages/listingInfo/Slider.tsx
+++ client/src/pages/listingInfo/Slider.tsx
@@ -2,7 +2,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { IListing, Image } from "../../../types";
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 
 interface Item {
   item: IListing | undefined;
